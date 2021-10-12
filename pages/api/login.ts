28941886e3a1de0a994ad87bf8397c32657f40e6
André Luiz Soares dos Santos <andresoares37@gmail.com>
2021-10-12 14:33:49 -0300

    add arquivos

---
+++ pages/api/login.ts
@@ -0,0 +1,31 @@
+import type {NextApiRequest, NextApiResponse} from 'next';
+import {Login} from '../../types/Login';
+import { DefaultResponseMsg } from '../../types/DefautResponseMsg';
+
+export default function handler (req : NextApiRequest, res: NextApiResponse<DefaultResponseMsg>)
+{
+    try
+    {
+        if(req.method !== 'POST')
+        {
+            res.status(400).json({error: 'Metodo solicitado não existe'});
+            return;
+        }
+
+        if(req.body)
+        {
+            const body = req.body as Login;        
+            if(body.login && body.password)
+            {
+                res.status(200).json({msg : 'Login efetuado com sucesso'});
+                return;
+            }
+        }
+        res.status(400).json({error: 'Usuário ou senha inválidos'});        
+    }
+    catch(e)
+    {
+        console.log('Ocorreu erro ao autenticar usuario: ', e);
+        res.status(500).json({error: 'Ocorreu erro ao autenticar usuario , tente novamente '});
+    }
+}
