7c8d2cedda268e99a7dbd2a06827b7954d241fef
andresoares37 <andresoares37@gmail.com>
2021-10-15 13:25:53 -0300

    LoginResponse

--- pages/api/login.ts
+++ pages/api/login.ts
@@ -1,29 +1,34 @@
 import type {NextApiRequest, NextApiResponse} from 'next';
-import { UserModel } from '../../models/UserModel';
-import {Login} from '../../types/Login';
-import { DefaultResponseMsg } from '../../types/DefautResponseMsg';
-import connectDB from '../../middlewares/connectDB';
 import md5 from 'md5';
-import { User } from '../../types/User';
+import jwt from 'jsonwebtoken';
+import connectDB from '../../middlewares/connectDB';
+import { UserModel } from '../../models/UserModel';
+import { DefaultResponseMsg } from '../../types/DefautResponseMsg';
+import {Login} from '../../types/Login';
+import {LoginResponse} from '../../types/LoginResponse';
 
-const handler = async (req : NextApiRequest, res: NextApiResponse<DefaultResponseMsg>) =>{
+const handler = async (req : NextApiRequest, res : NextApiResponse<DefaultResponseMsg | LoginResponse>) =>{
     try
     {
-        if(req.method !== 'POST')
-        {
+        if(req.method !== 'POST'){
             res.status(400).json({error: 'Metodo solicitado não existe'});
             return;
         }
 
-        if(req.body)
-        {
-            const auth = req.body as Login;        
+        const {MY_SECRET_KEY} = process.env;
+        if(!MY_SECRET_KEY){
+            res.status(500).json({ error: 'ENV my secret key nao encontrada '});
+            return;
+        }
+
+        if(req.body){
+            const auth = req.body as Login;
             if(auth.login && auth.password){
-                
                 const usersFound = await UserModel.find({email : auth.login, password: md5(auth.password)});
                 if(usersFound && usersFound.length > 0){
                     const user = usersFound[0];
-                    res.status(200).json(user);
+                    const token = jwt.sign({_id : user._id}, MY_SECRET_KEY);
+                    res.status(200).json({ token, name: user.name, email: user.email});
                     return;
                 }
             }
